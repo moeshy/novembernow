9538c342110ea192a42f6cdb232c427da1d22c95
Luke <103851112+moeshy@users.noreply.github.com>
2022-08-29 22:51:35 +0100

    working site

--- novembernow/src/pages/Layout.js
+++ novembernow/src/pages/Layout.js
@@ -1,6 +1,7 @@
 import NovvyNow from "../images/NovvyNow.png";
 import NN from "../images/NN.png";
 import "../components/app/App.css";
+import { Link } from "react-router-dom";
 
 
 const Layout = () => {
@@ -8,6 +9,12 @@ const Layout = () => {
             <body className="novBody">
               <div className="novDiv">
                 <img className="novSplash" src={NovvyNow} alt="Nov" />
+              </div>
+              <div className="navButtons">
+              <Link to="/profile" style={{ textDecoration: "none", color: "black" }}>HOME</Link>
+              <Link to="/profile" style={{ textDecoration: "none", color: "black" }}>ABOUT</Link>
+              <Link to="/profile" style={{ textDecoration: "none", color: "black" }}>MUSIC</Link>
+              <Link to="/profile" style={{ textDecoration: "none", color: "black" }}>CONTACT</Link>
               </div>
               <div className="nnDiv">
                 <img src={NN} className="NN" alt="NN" />
